6956e667f04d10c5fe973a7526819fff5afe4b6d
joaogcfa <joaogcfa@al.insper.edu.br>
2020-05-18 19:33:09 -0300

    requisito 2

---
+++ src/main/java/br/pro/hashi/ensino/desagil/aps/model/model/And3Gate.java
@@ -0,0 +1,44 @@
+package br.pro.hashi.ensino.desagil.aps.model.model;
+
+public class And3Gate extends Gate {
+    private final NandGate nand1;
+    private final NandGate nand2;
+    private final NandGate nand3;
+    private final NandGate nand4;
+
+    public And3Gate() {
+        super("AND3GATE", 3);
+        nand1 = new NandGate();
+        nand2 = new NandGate();
+        nand3 = new NandGate();
+        nand4 = new NandGate();
+
+        nand2.connect(0, nand1);
+        nand2.connect(1, nand1);
+        nand3.connect(0, nand2);
+        nand4.connect(0, nand3);
+        nand4.connect(1, nand3);
+    }
+
+    @Override
+    public boolean read() {
+        return nand4.read();
+    }
+
+    @Override
+    public void connect(int inputIndex, Emitter emitter) {
+        if (inputIndex < 0 || inputIndex > 2) {
+            throw new IndexOutOfBoundsException(inputIndex);
+
+        } else if (inputIndex == 0) {
+            nand1.connect(0, emitter);
+
+        } else if (inputIndex == 1) {
+            nand1.connect(1, emitter);
+        }
+        else if (inputIndex == 2){
+            nand3.connect(1, emitter);
+        }
+    }
+}
+
